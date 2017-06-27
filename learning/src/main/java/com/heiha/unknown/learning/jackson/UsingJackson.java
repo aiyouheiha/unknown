b112ace26f9a0360cc8607cad6b76236b69fb4e1
heiha <aiyouheiha@163.com>
2017-06-27 11:58:34 +0800

    add jackson using sample

--- learning/src/main/java/com/heiha/unknown/learning/jackson/UsingJackson.java
+++ learning/src/main/java/com/heiha/unknown/learning/jackson/UsingJackson.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.heiha.unknown.learning.pojo.User;
 
+import java.io.IOException;
+
 /**
  * <br>
  * <b>Project:</b> unknown<br>
@@ -12,13 +14,20 @@ import com.heiha.unknown.learning.pojo.User;
  * <b>Author:</b> heiha<br>
  */
 public class UsingJackson {
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void main(String[] args) throws IOException {
         User user = new User();
         user.setAge(10);
         user.setIsMaster(true);
         user.setUserName("abc");
 
+        ObjectMapper mapper = new ObjectMapper();
 
+        String userStr = mapper.writeValueAsString(user);
+        System.out.println(userStr);
+        User userTrans = mapper.readValue(userStr, User.class);
+        System.out.println(userTrans.getAge());
+        System.out.println(userTrans.getIsMaster());
+        System.out.println(userTrans.getUserName());
     }
 
 
